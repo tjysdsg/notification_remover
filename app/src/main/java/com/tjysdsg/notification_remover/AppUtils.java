aa45b9bdbf6c5e96ccd5d54f86fd4a57fd5f691a
tjysdsg <tjy1018543509@163.com>
2023-05-27 10:30:29 -0400

    Update build script

--- app/src/main/java/com/tjysdsg/notification_remover/AppUtils.java
+++ app/src/main/java/com/tjysdsg/notification_remover/AppUtils.java
@@ -1,7 +1,9 @@
 package com.tjysdsg.notification_remover;
 
+import android.app.ActivityManager;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.util.Log;
 
 public class AppUtils {
     public static String getAppName(Context context, String packageName) throws PackageManager.NameNotFoundException {
@@ -9,6 +11,17 @@ public class AppUtils {
         return (String) packageManager.getApplicationLabel(
                 packageManager.getApplicationInfo(packageName, PackageManager.GET_META_DATA)
         );
+    }
+
+    public static boolean isServiceRunning(Context context, Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            Log.d("AppUtils.isServiceRunning", "Running service - " + service.service.getClassName());
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
