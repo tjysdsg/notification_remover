7b20e01ff65a3320691d0f6abbd231582ac4e0f9
tjysdsg <tjy1018543509@163.com>
2023-06-23 17:01:55 -0400

    Extract notification listener service related functionality to a separate class rather than main acitivity

--- app/src/main/java/com/tjysdsg/notification_remover/MainActivity.java
+++ app/src/main/java/com/tjysdsg/notification_remover/MainActivity.java
@@ -2,34 +2,18 @@ package com.tjysdsg.notification_remover;
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.IBinder;
-import android.provider.Settings;
-import android.text.TextUtils;
-import android.content.ComponentName;
-import android.util.Log;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
-public class MainActivity extends AppCompatActivity implements
-        SwipeRefreshLayout.OnRefreshListener,
-        INotificationListenerActivity {
+public class MainActivity extends NotificationListenerActivity implements SwipeRefreshLayout.OnRefreshListener {
 
-    private static final String ENABLED_NOTIFICATION_LISTENERS = "enabled_notification_listeners";
-    private static final String ACTION_NOTIFICATION_LISTENER_SETTINGS = "android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS";
     SwipeRefreshLayout swipeRefreshLayout;
     RecyclerView notificationList;
     NotificationListAdapter notificationListAdapter;
     NotificationListener notificationListener;
-    Intent notificationListenerServiceIntent;
-    NotificationServiceConnection notificationServiceConnection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,62 +25,6 @@ public class MainActivity extends AppCompatActivity implements
         swipeRefreshLayout.setOnRefreshListener(this);
 
         notificationList = findViewById(R.id.notification_list);
-
-        notificationServiceConnection = new NotificationServiceConnection(this);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        // Ask user for permission to manage notifications
-        if (isNotificationServiceEnabled()) {
-            startNotificationListenerService();
-        } else {
-            AlertDialog enableNotificationListenerAlertDialog = buildNotificationServiceAlertDialog();
-            enableNotificationListenerAlertDialog.show();
-        }
-    }
-
-    /**
-     * Start the notification listener service. Do nothing if it's already running
-     */
-    public void startNotificationListenerService() {
-        assert isNotificationServiceEnabled();
-
-        if (notificationServiceConnection.isConnected()) return;
-
-        notificationListenerServiceIntent = new Intent(
-                this,
-                NotificationListener.class
-        );
-        boolean res = bindService(
-                notificationListenerServiceIntent, notificationServiceConnection,
-                Context.BIND_AUTO_CREATE
-        );
-
-        if (!res) {
-            // TODO: show error dialog
-            throw new RuntimeException("Failed to bindService");
-        }
-    }
-
-    /**
-     * Stop the notification service if it's running.
-     */
-    public void stopNotificationListenerService() {
-        if (notificationServiceConnection.isConnected()) {
-            unbindService(notificationServiceConnection);
-
-            boolean stopped = stopService(notificationListenerServiceIntent);
-            Log.e("MainActivity", "stopService returned " + stopped);
-        }
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        stopNotificationListenerService();
     }
 
     // Swipe down to refresh
@@ -109,36 +37,8 @@ public class MainActivity extends AppCompatActivity implements
         swipeRefreshLayout.setRefreshing(false);
     }
 
-    /**
-     * Check whether the notification listener service is enabled.
-     *
-     * @return True if enabled, false otherwise.
-     */
-    private boolean isNotificationServiceEnabled() {
-        String pkgName = getPackageName();
-        final String flat = Settings.Secure.getString(getContentResolver(), ENABLED_NOTIFICATION_LISTENERS);
-        if (!TextUtils.isEmpty(flat)) {
-            final String[] names = flat.split(":");
-            for (String name : names) {
-                final ComponentName cn = ComponentName.unflattenFromString(name);
-                if (cn != null) {
-                    if (TextUtils.equals(pkgName, cn.getPackageName())) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-    /**
-     * Build Notification Listener Alert Dialog.
-     * Builds the alert dialog that pops up if the user has not turned
-     * the Notification Listener Service on yet.
-     *
-     * @return An alert dialog which leads to the notification enabling screen
-     */
-    private AlertDialog buildNotificationServiceAlertDialog() {
+    @Override
+    protected void askForNotificationServicePermission() {
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
         alertDialogBuilder.setTitle(R.string.notification_listener_service);
         alertDialogBuilder.setMessage(R.string.notification_listener_service_explanation);
@@ -151,11 +51,9 @@ public class MainActivity extends AppCompatActivity implements
                     // the app. will not work as expected
                 }
         );
-        return (alertDialogBuilder.create());
-    }
 
-    private void jumpToNotificationServicePermissionSettingPage() {
-        startActivity(new Intent(ACTION_NOTIFICATION_LISTENER_SETTINGS));
+        var dialog = (alertDialogBuilder.create());
+        dialog.show();
     }
 
     @Override
---
+++ app/src/main/java/com/tjysdsg/notification_remover/NotificationListenerActivity.java
@@ -0,0 +1,108 @@
+package com.tjysdsg.notification_remover;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.content.ComponentName;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+abstract public class NotificationListenerActivity extends AppCompatActivity implements INotificationListenerActivity {
+
+    private static final String ENABLED_NOTIFICATION_LISTENERS = "enabled_notification_listeners";
+    private static final String ACTION_NOTIFICATION_LISTENER_SETTINGS = "android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS";
+    Intent notificationListenerServiceIntent;
+    NotificationServiceConnection notificationServiceConnection;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        notificationServiceConnection = new NotificationServiceConnection(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Ask user for permission to manage notifications
+        if (hasNotificationServicePermission()) {
+            startNotificationListenerService();
+        } else {
+            askForNotificationServicePermission();
+        }
+    }
+
+    protected abstract void askForNotificationServicePermission();
+
+    /**
+     * Start the notification listener service. Do nothing if it's already running
+     */
+    public void startNotificationListenerService() {
+        assert hasNotificationServicePermission();
+
+        if (notificationServiceConnection.isConnected()) return;
+
+        notificationListenerServiceIntent = new Intent(
+                this,
+                NotificationListener.class
+        );
+        boolean res = bindService(
+                notificationListenerServiceIntent, notificationServiceConnection,
+                Context.BIND_AUTO_CREATE
+        );
+
+        if (!res) {
+            // TODO: show error dialog
+            throw new RuntimeException("Failed to bindService");
+        }
+    }
+
+    /**
+     * Stop the notification service if it's running.
+     */
+    public void stopNotificationListenerService() {
+        if (notificationServiceConnection.isConnected()) {
+            unbindService(notificationServiceConnection);
+
+            boolean stopped = stopService(notificationListenerServiceIntent);
+            Log.e(getClass().getName(), "stopService returned " + stopped);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        stopNotificationListenerService();
+    }
+
+    /**
+     * Check whether the notification listener service is enabled.
+     *
+     * @return True if enabled, false otherwise.
+     */
+    protected boolean hasNotificationServicePermission() {
+        String pkgName = getPackageName();
+        final String flat = Settings.Secure.getString(getContentResolver(), ENABLED_NOTIFICATION_LISTENERS);
+        if (!TextUtils.isEmpty(flat)) {
+            final String[] names = flat.split(":");
+            for (String name : names) {
+                final ComponentName cn = ComponentName.unflattenFromString(name);
+                if (cn != null) {
+                    if (TextUtils.equals(pkgName, cn.getPackageName())) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    protected void jumpToNotificationServicePermissionSettingPage() {
+        startActivity(new Intent(ACTION_NOTIFICATION_LISTENER_SETTINGS));
+    }
+
+}
