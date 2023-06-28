4e61ee04a8e2c41ea97feb6d2919f51b52ffd0ad
tjysdsg <tjy1018543509@163.com>
2023-06-28 19:49:00 -0400

    Refresh notification list as soon as the NotificationListener is connected

--- app/src/main/java/com/tjysdsg/notification_remover/MainActivity.java
+++ app/src/main/java/com/tjysdsg/notification_remover/MainActivity.java
@@ -72,6 +72,7 @@ public class MainActivity extends NotificationListenerActivity implements SwipeR
         notificationListener.registerListenerCallback(
                 (l) -> updateMainScreen()
         );
+        updateMainScreen();
     }
 
     @Override
